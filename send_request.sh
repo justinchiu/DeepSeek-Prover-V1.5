2f69bb22e002a91bd48f028fd6549d10e2775607
justinchiu-cohere <justinchiu@cohere.com>
2024-09-06 15:10:00 -0400

    change url

--- send_request.sh
+++ send_request.sh
@@ -1,4 +1,4 @@
 curl -X POST -H 'Content-Type: application/json' \
     --data-binary '[{"code": "import Mathlib"}]' \
-    https://justinchiu--verifier-verify-dev.modal.run
+    https://justinchiu--verifier-verify.modal.run
 
